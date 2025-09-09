0a1abe9ec44c71c32088f55ed66ba333ba76adde
The_Borts <bortolettolucas2005@gmail.com>
2025-09-09 09:46:45 -0300

    Fixing some minor issues on the LoginSignUp html to facilitate testing

--- saude_e_bem_estar/src/components/login_sign_up/LoginSignUp.jsx
+++ saude_e_bem_estar/src/components/login_sign_up/LoginSignUp.jsx
@@ -17,24 +17,24 @@ const LoginSignUp = () => {
             <div className='inputs'>
                 {action==="Login"?<div></div>:
                 <div className='input'>
-                    <img src={user_icon} alt="" />
+                    <img src={user_icon} alt="user_icon" />
                     <input type="text" placeholder='Name'/>
                 </div>}
 
                 <div className='input'>
-                    <img src={email_icon} alt="" />
+                    <img src={email_icon} alt="email_icon" />
                     <input type="email" placeholder='Email'/>
                 </div>
                 <div className='input'>
-                    <img src={password_icon} alt="" />
+                    <img src={password_icon} alt="password_icon" />
                     <input type="password" placeholder='Password'/>
                 </div>
             </div>
             {action==="Sign Up"?<div></div>:
             <div className="forgot-password">Lost Password? <span>Click Here</span></div>}
             <div className="submit-container">
-                <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-                <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+                <div role={"button"} className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+                <div role={"button"} className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
             </div>
         </div>
     );
